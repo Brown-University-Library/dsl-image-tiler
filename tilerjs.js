f6d44cba3b03566f0376464d06149d62c58d363f
Allison Hamburger <aghamburger@gmail.com>
2013-09-27 10:34:56 -0400

    Forgot 2 files

---
+++ tilerjs.js
@@ -0,0 +1,9 @@
+
+
+
+var canvas = document.getElementById('wall');
+var context=canvas.getContext();
+
+image = new Image();
+image.src = 'dog.jpg';
+image.onloadcontext.drawImage(image, 100 ,100);
