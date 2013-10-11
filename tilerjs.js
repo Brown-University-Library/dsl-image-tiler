5b3c3a6bb2c624c3c6cd4b6c2b3d5a988d259004
Patrick Rashleigh <edward_rashleigh@brown.edu>
2013-10-11 12:16:33 -0400

    Changed absolute path to relative

--- tilerjs.js
+++ tilerjs.js
@@ -1,84 +1,48 @@
-
-/*
-<canvas id="wall" width="1200" height="1200"
-    style="border:1px solid #000000;">
-  </canvas>
-*/
-
-//var canvas  = document.getElementById('wall');
+//VLC command: /Applications/VLC.app/Contents/MacOS/VLC ./Documents/DSL/dsl-image-tiler/citylights.mp4 --rate=1 --video-filter=scene 
+//              --vout=dummy --start-time=0 --stop-time=1000 --scene-format=jpg --scene-ratio=24 
+//              --scene-prefix=snap --scene-path=/Volumes/Flashdrive/DSL/
 
 
+var ON_DISPLAY_WALL = false; // set this to true when testing on the display wall
+var ON_WEB_SERVER   = false; // if running in a web server, set to true
 
-/*
-image     = new Image();
-image.src = 'dog.jpg';
-
-image.onload = function() {
-	context.drawImage(this, 0,0, 200, 100);
-};*/
-
-/*var sources = {
-	dog:'dog.jpg', 
-	trees:'trees.jpg'
-};
-
-
-function loadImages(sources, callback){
-
-	var loadedImages = 0;
-	var images = {};
-		for (var src in sources){
-			images[src] = new Image();
-			images[src].onload = function() {
-				context.drawImage(this, 0, 0, 200, 100);
-				if(++loadedImages >= 2) {
-              		callback(images);
-            	}
-		  		
-	  		};
-	  		images[src].src = sources[src];
-	  	}
-	}
-
-}
-*/
-var canvas = document.createElement('canvas')
-canvas.width = 1500;
-canvas.height = 1500;
+var canvas    = document.createElement('canvas')
+canvas.width  = ON_DISPLAY_WALL ? 1920 * 4 : 1200;
+canvas.height = ON_DISPLAY_WALL ? 1080 * 3 : 1200;
 
 var context = canvas.getContext('2d');
 
-//fake values for now
-var WALLHEIGHT = 3240;
-var WALLWIDTH = 7680/2;
-var BEZEL = 10;
-var SCREENWIDTH = 300;
-var SCREENHEIGHT = 100;
+var BEZEL        = 0;
+var SCREENWIDTH  = ON_DISPLAY_WALL ? 1920 : 300;
+var SCREENHEIGHT = ON_DISPLAY_WALL ? 1080 : 100;
 
-/* Varies by project */
-var numImgs = 6;	
-var srcDir = "/Users/Allison/Documents/DSL/dsl-image-tiler/photos2/";
-var basename = "photo";
+
+
+// If running on a server, use relative addresses
+var srcDir    = (ON_WEB_SERVER ? '' : '/Volumes/Flashdrive/DSL/converted/');
+var basename  = "snap";
 var extension = ".jpg";
 
+/* Varies by project */
+var numImgs = 946;  
+var pwidth  = 150/2;
+var pheight = 50;
 
-var pwidth = 150;
-var pheight = 100;
-
-var cols = 4;
+var cols = 60;
 
 for (var i = 0; i < numImgs; i++)
 {
    (function(j){
-      var imgSrc = srcDir + basename + (j+1) + extension;
-      var img = new Image();
-      img.onload = function() {
+        var imgSrc = srcDir + basename + (j+1) + extension;
+        var img    = new Image();
+      
+        img.onload = function() {
 
-      	screenCol = Math.floor(pwidth * (j % cols) / SCREENWIDTH);
-		screenRow = Math.floor(pheight * (Math.floor(j / cols)) / SCREENHEIGHT);
+        screenCol = Math.floor(pwidth * (j % cols) / SCREENWIDTH);
+        screenRow = Math.floor(pheight * (Math.floor(j / cols)) / SCREENHEIGHT);
 
-      	x = pwidth * (j % cols) + (BEZEL * screenCol);
-      	y = pheight * (Math.floor(j / cols))+ (BEZEL * screenRow);
+        x = pwidth * (j % cols) + (BEZEL * screenCol);
+        y = pheight * (Math.floor(j / cols))+ (BEZEL * screenRow);
         
         context.drawImage(img, x, y, pwidth, pheight);
       }
